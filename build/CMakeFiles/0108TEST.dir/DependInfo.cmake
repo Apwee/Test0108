01284c86ed5471ccc595a7f0aab7f682faf67c71
Apwee <144507706+Apwee@users.noreply.github.com>
2026-08-01 12:17:42 -0300

    CORRECCIÓN DE ERRORES: No se desarrollaba el ejecutable porque me olvidé de incluír LIB_DIR, la lista de librerias que existen dentro de la carpeta lib
    Actualización en el archivo: CMakeLists
    
    -Se incluyen las librerías. Se usa LIB_DIR dentro de target_link_libraries

--- build/CMakeFiles/0108TEST.dir/DependInfo.cmake
+++ build/CMakeFiles/0108TEST.dir/DependInfo.cmake
@@ -9,6 +9,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/zfrxn/Documents/GitHub/Test0108/src/glad.c" "CMakeFiles/0108TEST.dir/src/glad.c.obj" "gcc" "CMakeFiles/0108TEST.dir/src/glad.c.obj.d"
+  "C:/Users/zfrxn/Documents/GitHub/Test0108/src/main.cpp" "CMakeFiles/0108TEST.dir/src/main.cpp.obj" "gcc" "CMakeFiles/0108TEST.dir/src/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/CMakeFiles/0108TEST.dir/cmake_clean.cmake
+++ build/CMakeFiles/0108TEST.dir/cmake_clean.cmake
@@ -4,10 +4,12 @@ file(REMOVE_RECURSE
   "0108TEST.pdb"
   "CMakeFiles/0108TEST.dir/src/glad.c.obj"
   "CMakeFiles/0108TEST.dir/src/glad.c.obj.d"
+  "CMakeFiles/0108TEST.dir/src/main.cpp.obj"
+  "CMakeFiles/0108TEST.dir/src/main.cpp.obj.d"
   "lib0108TEST.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang C CXX)
   include(CMakeFiles/0108TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
